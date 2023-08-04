7c81e1277f434844965ea6311ce6c5e218a504c3
João Pinheiro <joaomh@protonmail.com>
2023-08-04 09:40:24 -0300

    Create reward_function.py

---
+++ reward_function.py
@@ -0,0 +1,20 @@
+def reward_function(params):
+  '''
+  Example of rewarding the agent to follow center line
+  '''
+
+  track_width = params['track_width']
+  distance_from_center = params['distance_from_center']
+
+  marker_1 = 0.1*track_width
+  marker_2 = 0.25*track_width
+  marker_3 = 0.5*track_width
+  if distance_from_center ≤ marker_1:
+    reward = 1
+  elif distance_from_center ≤ marker_2:
+    reward = 0.5
+  elif distance_from_center ≤ marker_3:
+    reward = 0.1
+  else:
+    reward = 1e-3 # likely crashed/ close to off track
+  return reward
